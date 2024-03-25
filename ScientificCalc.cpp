8dae9ad9ccc41e7b60b411ffb2535a7f580d1dd9
Lopes <g.melo.lopes9@gmail.com>
2024-03-25 11:45:32 +0000

    novas comedias

---
+++ ScientificCalc.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "SimpleCalc.h"
+
+using namespace std;
+
+int main(){
+    SimpleCalculator Calculator;
+
+    char op;
+    double num1,num2,result;
+
+    cout << "Enter number1: ";
+    cin >> num1;
+    cout << "Enter operator: ";
+    cin >> op;
+    cout << "Enter number2: ";
+    cin >> num2;
+
+    switch(op){
+        case '+':
+        result = Calculator.add(num1,num2);
+        break;
+
+        case '-':
+        result = Calculator.sub(num1,num2);
+        break;
+
+        case '*':
+        result = Calculator.mul(num1,num2);
+        break;
+
+        case '/':
+        result = Calculator.div(num1,num2);
+        break;
+
+        default:
+        cout << "Error!! invalid operator";
+        //return -1;
+        return 0;
+    }
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
---
+++ SimpleCalc.cpp
@@ -0,0 +1,8 @@
+
+
+#include "SimpleCalc.h"
+
+double SimpleCalculator::add(double num1,double num2){return num1 + num2;}
+double SimpleCalculator::sub(double num1,double num2){return num1 - num2;}
+double SimpleCalculator::mul(double num1,double num2){return num1 * num2;}
+double SimpleCalculator::div(double num1,double num2){return num1 / num2;}
