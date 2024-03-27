e21e23996f7452410efd1f69c7828829348a8261
Lopes <g.melo.lopes9@gmail.com>
2024-03-27 14:08:18 +0000

    hey

--- Test.cpp
+++ Test.cpp
@@ -3,23 +3,38 @@
 #include "catch.hpp"
 #include "SimpleCalc.h"
 
-TEST_CASE("Addition") {
+TEST_CASE("Arithmetic Operations") {
     SimpleCalc calculator;
-    REQUIRE(calculator.add(1.0, 2.0) == 3.0);
-}
 
-TEST_CASE("Subtraction") {
-    SimpleCalc calculator;
-    REQUIRE(calculator.sub(3.0, 2.0) == 1.0);
-}
+    // Addition
+    SECTION("Addition") {
+        REQUIRE(calculator.add(0.0, 0.0) == 0.0);
+        REQUIRE(calculator.add(1.0, 2.0) == 3.0);
+        REQUIRE(calculator.add(-1.0, 1.0) == 0.0);
+        REQUIRE(calculator.add(-1.0, -2.0) == -3.0);
+    }
 
-TEST_CASE("Multiplication") {
-    SimpleCalc calculator;
-    REQUIRE(calculator.mul(3.0, 2.0) == 6.0);
-}
+    // Subtraction
+    SECTION("Subtraction") {
+        REQUIRE(calculator.sub(0.0, 0.0) == 0.0);
+        REQUIRE(calculator.sub(5.0, 2.0) == 3.0);
+        REQUIRE(calculator.sub(-1.0, 1.0) == -2.0);
+        REQUIRE(calculator.sub(-1.0, -2.0) == 1.0);
+    }
 
-TEST_CASE("Division") {
-    SimpleCalc calculator;
-    REQUIRE(calculator.div(6.0, 2.0) == 3.0);
-   // REQUIRE_THROWS_WITH(calculator.div(6.0, 0.0), "Error! Division by zero is not allowed.");
+    // Multiplication
+    SECTION("Multiplication") {
+        REQUIRE(calculator.mul(0.0, 0.0) == 0.0);
+        REQUIRE(calculator.mul(3.0, 2.0) == 6.0);
+        REQUIRE(calculator.mul(-1.0, 2.0) == -2.0);
+        REQUIRE(calculator.mul(-2.0, -2.0) == 4.0);
+    }
+
+    // Division
+    SECTION("Division") {
+        REQUIRE(calculator.div(0.0, 1.0) == 0.0);
+        REQUIRE(calculator.div(6.0, 2.0) == 3.0);
+        REQUIRE(calculator.div(-8.0, 2.0) == -4.0);
+        //REQUIRE_THROWS_WITH(calculator.div(5.0, 0.0), "Division by zero should throw an exception");
+    }
 }
